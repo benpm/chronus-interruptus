0c7088525b4705609a989e9856b29244d3096709
Lemoncreme <schwalegos@gmail.com>
2018-11-17 20:18:11 -0700

    paths are working now

--- js/entities/hazards.js
+++ js/entities/hazards.js
@@ -1,9 +1,45 @@
 game.Path = me.Polygon.extend({
-	init: function (x, y, points) {
+	init: function (x, y, data) {
+		var vectors = [];
+		for (const point of data.points) {
+			vectors.push(new me.Vector2d(point.x, point.y));
+		}
 		this._super(me.Polygon, "init", [
-			x,y,points
+			x,y,vectors
 		]);
-		console.log(x, y, points);
+		this.name = data.id;
+		this.length = 0;
+		this.map = [];
+		for (let i = 0; i < vectors.length - 1; i++) {
+			const prev = this.length;
+			this.length += vectors[i].distance(vectors[i + 1]);
+			this.map.push({
+				a: prev,
+				b: this.length,
+				diff: this.length - prev,
+				v1: vectors[i],
+				v2: vectors[i + 1]
+			});
+		}
+		console.log(this.map);
+	},
+
+	update: function (params) {
+		//console.log(params);
+	},
+
+	posAt: function (vec, pixelAlong) {
+		for (const range of this.map) {
+			if (pixelAlong >= range.a && pixelAlong < range.b) {
+				const alongVector = range.diff - (range.b - pixelAlong);
+				const angle = Math.atan2(
+					/* this.pos.y * 2 +  */range.v2.y - range.v1.y, 
+					/* this.pos.x * 2 +  */range.v2.x - range.v1.x);
+				vec.x = range.v1.x + this.pos.x + (Math.cos(angle) * alongVector);
+				vec.y = range.v1.y + this.pos.y + (Math.sin(angle) * alongVector);
+				return;
+			}
+		}
 	}
 });
 
@@ -29,19 +65,39 @@ game.Hazard = me.Sprite.extend({
 		this.rangemax = this.pos.x + settings.max_x;
 		this.timerange = Math.abs(this.rangemax - this.rangemin);
 		this.moverange = settings.moverange;
+		this.pathname = settings.path;
+		this.curpoint = 0;
+		this.direction = 1;
+		this.speed = 2;
 	},
 
 	/* onCollision: function (response, other) {
 		return true;
 	}, */
 
-	/* update: function (dt) {
-		this.pos.x = this.initx;
-		this.pos.y = this.inity;
-		this.body.update(dt);
-		me.collision.check(this);
+	update: function (dt) {
+		//Find path
+		if (this.pathname && !this.path) {
+			this.path = me.game.world.getChildByName(this.pathname)[0];
+		}
+		if (this.path) {
+			/* const targetx = this.path.pos.x + this.path.points[this.curpoint].x;
+			const targety = this.path.pos.y + this.path.points[this.curpoint].y;
+			const distx = this.pos.x - targetx;
+			const disty = this.pos.y - targety;
+			this.pos.x += distx < this.speed ? this.speed : distx > this.speed ? -this.speed : 0;
+			this.pos.y += disty < this.speed ? this.speed : disty > this.speed ? -this.speed : 0;
+			if (Math.abs(distx) <= this.speed && Math.abs(disty) <= this.speed) {
+				if (this.curpoint == this.path.points.length - 1 || 
+					(this.curpoint == 0 && this.direction == -1))
+					this.direction = -this.direction;
+				this.curpoint += this.direction;
+			} */
+			this.path.posAt(this.pos, this.curpoint);
+			this.curpoint = (this.curpoint + 3) % this.path.length;
+		}
 		return (this._super(me.Entity, "update", [dt]));
-	} */
+	}
 });
 
 game.HazardWeight = game.Hazard.extend({
